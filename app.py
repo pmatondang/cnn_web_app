3d01563f62a7aa5cd7fcf61472ac13e712a43c8b
pmatondang <66526346+pmatondang@users.noreply.github.com>
2026-01-24 15:39:56 +0700

    update image clearly.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("Aplikasi Prediksi Gambar CNN")
+st.title("Aplikasi Sederhana Prediksi Gambar CNN")
 
 model = tf.keras.models.load_model("cnn_cifar10_model.h5")
 
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Gambar Input", use_column_width=True)
+    st.image(image, caption="Gambar Input (asli)", use_column_width=True)
 
     img = image.resize((32,32))
     img = np.array(img) / 255.0
@@ -21,5 +21,7 @@ if uploaded_file:
 
     prediction = model.predict(img)
     st.success(f"Hasil Prediksi: {np.argmax(prediction)}")
+    
+    st.image(img.resize((128,128)), caption="Versi 32x32 diperbesar untuk model")
 
 
