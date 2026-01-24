3c8f04abb9ac30a75200f1091dcb1abb418e6ff5
pmatondang <66526346+pmatondang@users.noreply.github.com>
2026-01-24 15:41:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("Aplikasi Sederhana Prediksi Gambar CNN")
+st.title("Aplikasi Prediksi Gambar CNN")
 
 model = tf.keras.models.load_model("cnn_cifar10_model.h5")
 
@@ -15,13 +15,12 @@ if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Gambar Input (asli)", use_column_width=True)
 
-    img = image.resize((32,32))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
+    small_img = image.resize((32,32))
 
-    prediction = model.predict(img)
+    img_array = np.array(small_img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
     st.success(f"Hasil Prediksi: {np.argmax(prediction)}")
-    
-    st.image(img.resize((128,128)), caption="Versi 32x32 diperbesar untuk model")
 
-
+    st.image(small_img.resize((128,128)), caption="Versi 32x32 diperbesar untuk model")
